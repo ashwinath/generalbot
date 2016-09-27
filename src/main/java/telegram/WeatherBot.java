89e429fd2244bcbe039e9ff730e09418d027b276
ashwin <ashwinath@hotmail.com>
2016-09-28 00:11:11 +0800

    changed to more appropriate superclass

--- src/main/java/telegram/WeatherBot.java
+++ src/main/java/telegram/WeatherBot.java
@@ -3,34 +3,22 @@ package telegram;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.bots.TelegramLongPollingCommandBot;
 import telegram.utils.TelegramUtils;
-import weather.json.CountryWeather;
-import weather.utils.WeatherUtils;
 
 /**
  * Created by ashwin on 27/9/2016.
  */
-public class WeatherBot extends TelegramLongPollingCommandBot {
+public class WeatherBot extends TelegramLongPollingBot {
     private final static Logger LOGGER = LoggerFactory.getLogger(WeatherBot.class);
     private static final String BOT_USERNAME = "WeatherBot";
     private static final String BOT_TOKEN = "***REMOVED***";
 
-    public String getBotUsername() {
-        return this.BOT_USERNAME;
-    }
-
-    public String getBotToken() {
-        return this.BOT_TOKEN;
-    }
-
-    public void processNonCommandUpdate(Update update) {
+    public void onUpdateReceived(Update update) {
         if(update.hasMessage()) {
             Message message = update.getMessage();
 
@@ -46,5 +34,13 @@ public class WeatherBot extends TelegramLongPollingCommandBot {
                 }
             }
         }
+    }
+
+    public String getBotUsername() {
+        return this.BOT_USERNAME;
+    }
+
+    public String getBotToken() {
+        return this.BOT_TOKEN;
     }
 }
