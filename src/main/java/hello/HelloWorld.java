7978943c2005c245e31f24ff2fe05764f997a728
ashwin <ashwinath@hotmail.com>
2016-09-27 21:21:17 +0800

    Initial commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ashwin on 27/9/2016.
+ */
+public class HelloWorld {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HelloWorld.class);
+
+    public static void main(String... args) {
+        LOGGER.info("Hello World");
+    }
+}
