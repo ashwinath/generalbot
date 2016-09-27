3abd845ffd4169de039b77afa93fcb53ad39517e
ashwin <ashwinath@hotmail.com>
2016-09-27 21:08:59 +0800

    Initial commit

---
+++ src/test/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by ashwin on 27/9/2016.
+ */
+public class HelloWorld {
+    public static void main(String... args) {
+        System.out.println("Hello World");
+    }
+}
