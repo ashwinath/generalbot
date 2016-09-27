9e4b21537a06bff13837a6c84f572ea461ce302b
ashwin <ashwinath@hotmail.com>
2016-09-27 22:25:57 +0800

    json parsing working

--- src/test/java/weather/TestWeather.java
+++ src/test/java/weather/TestWeather.java
@@ -7,9 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 import weather.json.CountryWeather;
 
-/**
- * Created by ashwin on 27/9/2016.
- */
 public class TestWeather {
     private final static Logger LOGGER = LoggerFactory.getLogger(TestWeather.class);
     private static final String API_KEY = "API_KEY";
@@ -20,5 +17,14 @@ public class TestWeather {
         CountryWeather singapore = restTemplate
                 .getForObject("http://api.openweathermap.org/data/2.5/weather?apikey=" + this.API_KEY + "&q=singapore", CountryWeather.class);
         Assert.assertEquals("SG", singapore.getSys().getCountry());
+    }
+
+    @Test
+    public void testJsonArray() {
+        RestTemplate restTemplate = new RestTemplate();
+        CountryWeather singapore = restTemplate
+                .getForObject("http://api.openweathermap.org/data/2.5/weather?apikey=" + this.API_KEY + "&q=singapore", CountryWeather.class);
+        LOGGER.info(singapore.getWeather().get(0).getMain());
+        Assert.assertNotNull(singapore.getWeather().get(0).getMain());
     }
 }
