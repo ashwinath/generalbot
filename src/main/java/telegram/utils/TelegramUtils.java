aacb75b7d277f70d51fb8dc18b3f54a0aa113a7e
ashwin <ashwinath@hotmail.com>
2016-09-28 21:10:47 +0800

    fixed degree symbol

--- src/main/java/telegram/utils/TelegramUtils.java
+++ src/main/java/telegram/utils/TelegramUtils.java
@@ -5,6 +5,7 @@ import weather.json.CountryWeather;
 import weather.utils.WeatherUtils;
 
 public class TelegramUtils {
+    private static final String DEGREE_SYMBOL = "\u00b0";
     public static String getWeatherString(String country, String apiKey) throws IllegalArgumentException {
         RestTemplate restTemplate = new RestTemplate();
         CountryWeather countryWeather = restTemplate
@@ -15,10 +16,12 @@ public class TelegramUtils {
         StringBuilder sb = new StringBuilder();
         sb.append("The temperature today in " + countryWeather.getName() + ", " + countryWeather.getSys().getCountry() + " is: Hi: ")
                 .append(String.format("%.1f", countryWeather.getMain().getMaxTemp()))
-                .append("°C ")
+                .append(DEGREE_SYMBOL)
+                .append("C ")
                 .append("Lo: ")
                 .append(String.format("%.1f", countryWeather.getMain().getMinTemp()))
-                .append("°C");
+                .append(DEGREE_SYMBOL)
+                .append("C");
         return sb.toString();
     }
 }
